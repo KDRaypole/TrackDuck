9fb86b280e521e8db0037ab7d4b8f8b5ee4cd00e
Kobe <KDRaypole01@indianatech.net>
2019-03-10 20:26:53 -0400

    Remove Spotify Service config from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,6 @@ import { spotifyConfig } from '../config'
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  providers:  [SpotifyService, {
-    provide: "SpotifyConfig" ,
-      useValue: {
-        clientId: spotifyConfig.fire.clientId,
-        redirectUri: 'http://localhost:4200/login/callback',
-        scope: 'playlist-modify-private playlist-modify-public user-follow-modify user-follow-read playlist-read-private playlist-read-collaborative playlist-modify-public playlist-modify-private user-library-read user-library-modify user-read-private user-top-read user-read-recently-played',
-        authToken: localStorage.getItem('spotify-token')
-      }
-    }
-  ]
 })
 export class AppComponent {
 }
