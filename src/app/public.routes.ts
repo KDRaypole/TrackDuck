efbe0a2d84f1ff97f7ad055630b61797466cb02a
Adam Swanson <AMSwanson01@indianatech.net>
2019-04-10 12:38:34 -0400

    Add landing page
    Adds a landing page, about page, and changes the login page.

--- src/app/public.routes.ts
+++ src/app/public.routes.ts
@@ -1,11 +1,15 @@
 import { LoginComponent } from './components/login/login.component'
 import { HomeComponent } from './components/home/home.component'
+import { AboutComponent } from './components/about/about.component'
 import { CallbackComponent } from './components/callback/callback.component'
 import { Routes, RouterModule } from '@angular/router';
 
 export const PUBLIC_ROUTES: Routes = [
   {
     path: '', component: HomeComponent
+  },
+  {
+    path: 'about', component: AboutComponent
   },
   {
     path: 'login', component: LoginComponent
