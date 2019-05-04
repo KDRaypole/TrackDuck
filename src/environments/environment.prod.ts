a04ab026d080e346640815539c97ced8e20db588
Kobe <KDRaypole01@indianatech.net>
2019-05-04 14:32:15 -0400

    Move app to production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
+import * as process from 'process';
+
 export const environment = {
-  production: true
+  production: true,
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  redirectUri: 'https://trackduck.herokuapp.com/login/callback'
 };
