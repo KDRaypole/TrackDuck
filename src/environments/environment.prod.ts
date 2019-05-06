1c942aace3dba67a5df4376257aacc8cf2790a14
Kobe <KDRaypole01@indianatech.net>
2019-05-05 21:59:33 -0400

    Test client id

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import * as process from 'process';
 
 export const environment = {
   production: true,
-  clientId: process.env.CLIENT_ID,
-  clientSecret: process.env.CLIENT_SECRET,
+  clientId: 'e18aeff2866a401d9a0792c84c3f3562',
+  clientSecret: '77641fa43e934b88b427346eff257c03',
   redirectUri: 'https://trackduck.herokuapp.com/login/callback'
 };
